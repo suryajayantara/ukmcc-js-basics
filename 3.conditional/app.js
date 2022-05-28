6b6562b42e4dfe908c2cd3fb38df39f9d599f514
suryajayantara <ptsuryajayantara01@gmail.com>
2022-05-29 03:20:10 +0800

    :sparkles: adding conditional content

---
+++ 3.conditional/app.js
@@ -0,0 +1,58 @@
+
+/*
+    Conditiona Type : else if , switch
+
+    1) Else If => conditional checked by comparing something
+    const x = 20;
+    if(x === 20){
+        console.log('x is 20');
+    }
+    else if(x === "20"){
+        console.log("x is 20 but in String data type");
+    }
+    else{
+        console.log('x is not 20');
+    }
+
+    Protips : 
+    1) You can shorter a ifelse using tenary operation
+    example : const odd = x%2 == 0 ? true : false;
+
+    2) you can shorter ifelse when you checking a boolean type by
+    example 
+    if(isActive ){
+        console.log('active');
+    }else{
+        console.log('error');
+    }
+
+
+    2) Switch case => conditional compare by value only
+    const traficLight = 'green';
+    switch (traficLight) {
+        case 'red':
+            console.log('Stop the vehicle !');
+            break;
+        case 'yellow':
+            console.log('Move your vehicle Slowly !');
+            break;
+        case 'green':
+            console.log('Move your vehicle');
+            break;
+        default:
+            console.log('Traffic Light is Off');
+            break;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
